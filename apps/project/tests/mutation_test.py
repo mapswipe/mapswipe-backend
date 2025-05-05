aa672167e9140649d2a710b9059fdfbfa32a5796
tnagorra <weathermist@gmail.com>
2025-05-05 19:19:05 +0545

    Remove unnecessary create_project_query function

--- apps/project/tests/mutation_test.py
+++ apps/project/tests/mutation_test.py
@@ -76,22 +76,6 @@ def create_project_aoi_asset_query(
         )
 
 
-def create_project_query(
-    *,
-    query_check_func: typing.Callable,
-    query: str,
-    project_data: dict,
-    **kwargs,
-) -> dict:
-    return query_check_func(
-        query,
-        variables={
-            "data": project_data,
-        },
-        **kwargs,
-    )
-
-
 def update_project_query(
     *,
     query_check_func: typing.Callable,
@@ -283,10 +267,11 @@ class TestProjectMutation(TestCase):
         )
 
     def _create_project_mutation(self, project_data: dict, **kwargs):
-        return create_project_query(
-            query_check_func=self.query_check,
+        return self.query_check(
             query=Mutation.CREATE_PROJECT,
-            project_data=project_data,
+            variables={
+                "data": project_data,
+            },
         )
 
     def _update_project_mutation(self, pk: str, project_data: dict, **kwargs):
@@ -613,11 +598,11 @@ class TestProjectTypeMutation(TestCase):
 
     def _create_project_mutation(self, project_data: dict, **kwargs):
         with self.captureOnCommitCallbacks(execute=True):
-            return create_project_query(
-                query_check_func=self.query_check,
+            return self.query_check(
                 query=Mutation.CREATE_PROJECT,
-                project_data=project_data,
-                **kwargs,
+                variables={
+                    "data": project_data,
+                },
             )
 
     def _update_project_mutation(self, pk: str, project_data: dict, **kwargs):
