b649b96ffbc20f0698714be3b1214eda1fb869d6
thenav56 <ayernavin@gmail.com>
2025-05-21 11:23:51 +0545

    feat(sentry): add sentry config override for celery beat tasks

--- main/celery.py
+++ main/celery.py
@@ -1,16 +1,23 @@
 import logging
 import os
+import typing
 from logging.config import dictConfig
 from typing import TYPE_CHECKING
 
 import celery
 from celery import signals
-from celery.schedules import crontab
 from django.conf import settings
+from django.db import models
 from kombu import Queue
 
+from .cronjobs import BEAT_SCHEDULES
+
 if TYPE_CHECKING:
+    from celery.app.task import Task
+
     from main.sentry import SentryConfig
+
+    Task.__class_getitem__ = classmethod(lambda cls, *args, **kwargs: cls)  # type: ignore[attr-defined]
 
 logger = logging.getLogger(__name__)
 
@@ -40,13 +47,7 @@ app.conf.task_queues = CeleryQueue.ALL_QUEUE
 # Load task modules from all registered Django apps.
 app.autodiscover_tasks()
 
-app.conf.beat_schedule = {
-    "apps.common.clear_expired_django_sessions": {
-        "task": "apps.common.clear_expired_django_sessions",
-        "schedule": crontab(minute="1", hour="1", day_of_week="1"),
-        "options": {"queue": "default"},
-    },
-}
+app.conf.beat_schedule = BEAT_SCHEDULES
 
 
 @signals.setup_logging.connect
@@ -56,6 +57,33 @@ def config_loggers(**_):
     dictConfig(settings.LOGGING)
 
 
+@signals.beat_init.connect
+def clean_up_periodic_tasks(**_):
+    try:
+        from django_celery_beat.models import PeriodicTask
+
+        obsolute_tasks_qs = PeriodicTask.objects.filter(
+            task__startswith="apps.",  # Our tasks
+        ).exclude(
+            models.Q(
+                name__in=list(BEAT_SCHEDULES.keys()),
+            )
+            | models.Q(
+                name__startswith="manual:",  # Lets filter-out if it has `manual:` at the start
+            ),
+        )
+
+        obsolute_tasks = list(obsolute_tasks_qs)
+        if obsolute_tasks:
+            for task in obsolute_tasks:
+                logger.warning("Task to delete: %s", task.name)
+
+            deleted_periodic_task = obsolute_tasks_qs.delete()
+            logger.warning("Deleted tasks not defined in the codebase: %s", deleted_periodic_task)
+    except Exception:
+        logger.error("Failed to clean-up PeriodicTasks", exc_info=True)
+
+
 @app.task(bind=True, ignore_result=True)
-def debug_task(self):
+def debug_task(self: "Task[typing.Any, typing.Any]"):
     logger.info("Request: %s", self.request)
