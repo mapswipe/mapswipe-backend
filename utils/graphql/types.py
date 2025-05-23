fb6c576caa7caf1e3831c56c6cde922f80f4b117
thenav56 <ayernavin@gmail.com>
2025-05-23 13:58:16 +0545

    fix(graphql): add custom file type to fix url issue

--- utils/graphql/types.py
+++ utils/graphql/types.py
@@ -3,7 +3,13 @@ import typing
 from typing import NewType
 
 import strawberry
+import strawberry_django
+from django.core.files.storage import FileSystemStorage, default_storage
+from django.db import models
+from django.db.models.fields import files
 from strawberry.scalars import JSON
+from strawberry.types import Info
+from strawberry_django.fields.types import field_type_map
 
 # generalize all the CustomErrorType
 CustomErrorType = strawberry.scalar(
@@ -43,4 +49,29 @@ GenericJSON = strawberry.scalar(
     NewType("GenericJSON", JSON),
     serialize=lambda v: json.loads(v) if isinstance(v, str) else v,
     parse_value=lambda v: v,
+)
+
+
+# Replaces strawberry_django.fields.types.DjangoFileType
+@strawberry.type
+class MapswipeDjangoFileType:
+    name: str
+    size: int
+
+    @strawberry_django.field
+    def url(
+        self,
+        info: Info,
+        file: strawberry.Parent[files.FieldFile],
+    ) -> str:
+        # TODO: Use cache if using S3 URL with signature
+        if isinstance(default_storage, FileSystemStorage):
+            return info.context.request.build_absolute_uri(file.url)
+        return file.url
+
+
+field_type_map.update(
+    {
+        models.FileField: MapswipeDjangoFileType,
+    },
 )
