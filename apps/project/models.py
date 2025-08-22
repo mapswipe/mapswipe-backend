3d68a0ddf350babcc1020737a121648b1a69f986
Sushil Tiwari <susiltiwari750@gmail.com>
2025-08-22 11:23:31 +0545

    feat(project): Add new field project instruction

--- apps/project/models.py
+++ apps/project/models.py
@@ -241,7 +241,9 @@ class Project(UserResource, FirebasePushResource):
 
     # TODO(tnagorra): Max length is 25 in manager dashboard.
     # TODO(frozenhelium): We should discuss if we need this field.
-    look_for = models.CharField[str, str](
+    look_for = models.CharField[str | None, str | None](
+        null=True,
+        blank=True,
         max_length=255,
         help_text=gettext_lazy("What should the users look for (e.g. buildings, cars, trees)"),
     )
@@ -260,10 +262,11 @@ class Project(UserResource, FirebasePushResource):
         blank=True,
     )  # NOTE: project_details before
 
-    # NOTE: Describe the mission of the project.
-    project_instruction = models.TextField(
+    project_instruction = models.TextField[str | None, str | None](
+        null=True,
+        blank=True,
         help_text=gettext_lazy(
-            "Provide mission statement.",
+            "Provide project instruction",
         ),
     )
 
