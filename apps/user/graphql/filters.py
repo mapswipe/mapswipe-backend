40f84810d2c70cc091134f38ccd9778df3c7cf8e
thenav56 <ayernavin@gmail.com>
2025-09-02 11:47:59 +0545

    feat(graphql): add contributor user in user type
    - add search and contributor user in user filters

--- apps/user/graphql/filters.py
+++ apps/user/graphql/filters.py
@@ -1,12 +1,31 @@
 import strawberry
 import strawberry_django
+from django.db import models
 
 from apps.common.filters import unaccented_filter
+from apps.contributor.graphql.filters import ContributorUserFilter
 from apps.user.models import User
 
 
 @strawberry_django.filters.filter(User, lookups=True)
 class UserFilter:
     id: strawberry.auto
-
+    contributor_user: ContributorUserFilter | None = strawberry.UNSET
     display_name = unaccented_filter("display_name")
+
+    @strawberry_django.filter_field
+    def search(
+        self,
+        queryset: models.QuerySet[User],
+        value: str,
+        prefix: str,
+    ) -> tuple[models.QuerySet[User], models.Q]:
+        return queryset, models.Q(
+            **{
+                f"{prefix}display_name__icontains": value,
+            },
+        ) | models.Q(
+            **{
+                f"{prefix}contributor_user__username__icontains": value,
+            },
+        )
--- apps/user/graphql/types.py
+++ apps/user/graphql/types.py
@@ -1,7 +1,12 @@
+import typing
+
 import strawberry
 import strawberry_django
 
 from apps.user.models import User
+
+if typing.TYPE_CHECKING:
+    from apps.contributor.graphql.types import ContributorUserType
 
 
 @strawberry_django.type(User)
@@ -11,6 +16,7 @@ class UserType:
     last_name: strawberry.auto
     display_name: strawberry.auto
     anonymize_email: str
+    contributor_user: typing.Annotated["ContributorUserType", strawberry.lazy("apps.contributor.graphql.types")] | None
 
 
 @strawberry_django.type(User)
