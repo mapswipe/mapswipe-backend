02bb7675a8c296e992c19d761be965c5037be827
tnagorra <weathermist@gmail.com>
2025-10-01 09:54:24 +0545

    fix(test): clear out tokens from generated cassette files

--- conftest.py
+++ conftest.py
@@ -1,4 +1,26 @@
+from urllib.parse import urlparse, urlunparse
+
 import pytest
+from django.http import QueryDict
+
+
+def scrub_auth_token(request):
+    # NOTE: We want to redact sensitize information from "Authorization: Token XYZ"
+    authorization_header = request.headers.get("authorization")
+    if authorization_header and authorization_header.lower().startswith("token"):
+        request.headers["authorization"] = "Token DUMMY_TOKEN"
+
+    url = request.uri
+    if "access_token" in url:
+        parsed_url = urlparse(url)
+
+        query_dict = QueryDict(parsed_url.query, mutable=True)
+        query_dict["access_token"] = "DUMMY_TOKEN"  # noqa: S105
+        new_query = query_dict.urlencode()
+
+        new_url = urlunparse(parsed_url._replace(query=new_query))
+        request.uri = new_url
+    return request
 
 
 @pytest.fixture(autouse=True)
@@ -14,4 +36,5 @@ def vcr_config(request):
         "ignore_hosts": ["localhost", "firebase-test"],
         "ignore_localhost": True,
         "cassette_library_dir": cassette_path or "assets/tests/tests_vcr_snapshots",
+        "before_record_request": scrub_auth_token,
     }
