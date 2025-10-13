bac54af88e16a0a38c15e531290b29f9818d78e7
thenav56 <ayernavin@gmail.com>
2025-10-13 10:26:37 +0545

    feat(user): add slack column and filter in admin panel

--- apps/user/admin.py
+++ apps/user/admin.py
@@ -1,6 +1,9 @@
+import typing
+
 from django import forms
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
+from django.utils.translation import gettext
 from django.utils.translation import gettext_lazy as _
 
 from .models import User
@@ -12,19 +15,46 @@ class UserCreationForm(forms.ModelForm):  # type: ignore[reportMissingTypeArgume
         fields = ["email"]
 
 
+class HasSlackUserIdFilter(admin.SimpleListFilter):
+    title = _("Slack User ID")
+    parameter_name = "has_slack_user_id"
+
+    @typing.override
+    def lookups(self, request, model_admin):
+        return (
+            ("yes", gettext("Has value")),
+            ("no", gettext("Is empty")),
+        )
+
+    @typing.override
+    def queryset(self, request, queryset):
+        if self.value() == "yes":
+            return queryset.exclude(slack_user_id__isnull=True).exclude(slack_user_id__exact="")
+        if self.value() == "no":
+            return queryset.filter(slack_user_id__isnull=True) | queryset.filter(slack_user_id__exact="")
+        return queryset
+
+
 @admin.register(User)
 class UserAdmin(DjangoUserAdmin):  # type: ignore[reportMissingTypeArgument]
     list_display = (
         "email",
         "first_name",
         "last_name",
+        "slack_user_id",
         "contributor_user__firebase_id",
         "contributor_user__username",
         "is_active",
         "is_staff",
         "is_superuser",
     )
-    list_filter = ("is_staff", "is_superuser", "is_active", "groups")
+    list_filter = (
+        "is_staff",
+        "is_superuser",
+        "is_active",
+        "groups",
+        HasSlackUserIdFilter,
+    )
     search_fields = ("email", "first_name", "last_name")
     ordering = ("email", "first_name", "last_name")
     add_form = UserCreationForm
