f73755506ff16e161ca6243858e3b6933252f0ad
ofritz <oliver.fritz@heigit.org>
2025-11-11 16:51:40 +0100

    feature(street): add image provider in schema

--- apps/project/graphql/types/project_types/street.py
+++ apps/project/graphql/types/project_types/street.py
@@ -7,5 +7,9 @@ from project_types.street import project as street_project
 class StreetMapillaryImageFilters: ...
 
 
+@strawberry.experimental.pydantic.type(model=street_project.StreetImageProvider, all_fields=True)
+class StreetImageProvider: ...
+
+
 @strawberry.experimental.pydantic.type(model=street_project.StreetProjectProperty, all_fields=True)
 class StreetProjectPropertyType: ...
