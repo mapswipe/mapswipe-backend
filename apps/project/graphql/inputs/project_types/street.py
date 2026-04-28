78d5b8e89d451f9d383befa5c1e2c903a03b4d10
ofritz <oliver.fritz@heigit.org>
2026-04-28 16:20:35 +0200

    feat(street): remove Mapillary from names

--- apps/project/graphql/inputs/project_types/street.py
+++ apps/project/graphql/inputs/project_types/street.py
@@ -4,8 +4,8 @@ from project_types.street import project as street_project
 from utils.geo.street_image_provider.models import StreetImageProvider
 
 
-@strawberry.experimental.pydantic.input(model=street_project.StreetMapillaryImageFilters, all_fields=True)
-class StreetMapillaryImageFiltersInput: ...
+@strawberry.experimental.pydantic.input(model=street_project.StreetImageFilters, all_fields=True)
+class StreetImageFiltersInput: ...
 
 
 @strawberry.experimental.pydantic.input(model=StreetImageProvider, all_fields=True)
--- apps/project/graphql/types/project_types/street.py
+++ apps/project/graphql/types/project_types/street.py
@@ -4,8 +4,8 @@ from project_types.street import project as street_project
 from utils.geo.street_image_provider.models import StreetImageProvider as StreetImageProviderModel
 
 
-@strawberry.experimental.pydantic.type(model=street_project.StreetMapillaryImageFilters, all_fields=True)
-class StreetMapillaryImageFilters: ...
+@strawberry.experimental.pydantic.type(model=street_project.StreetImageFilters, all_fields=True)
+class StreetImageFilters: ...
 
 
 @strawberry.experimental.pydantic.type(model=StreetImageProviderModel, all_fields=True)
